02a35908a0cdd6afc0703a43b2066d4f24202c0d
razibul-649 <razibul15-11649@diu.edu.bd>
2023-04-10 14:27:42 +0600

    subcategories updated by razib

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,6 +8,7 @@ class Admin extends CI_Controller {
         $this->load->helper('form');
         $this->load->database();
         $this->load->model('catmodel');
+        $this->load->model('subModel');
         $this->load->library('session');
         $this->load->helper('url');
     }
@@ -78,7 +79,51 @@ class Admin extends CI_Controller {
 	{
 
 
-
-		$this->load->view('admin/subcategories');
+        $categories = $this->catmodel->get_categories();
+		$data = array('categories' => $categories);
+        $this->load->view('admin/subcategories',$data);
 	}
+
+
+    public function subCatStor(){
+
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+		$this->form_validation->set_rules('subCat', ' SubCategory Name', 'required|max_length[12]|is_unique[Subcategories.subcname]');
+       
+
+
+        if ($this->form_validation->run() == FALSE) {
+			$this->session->set_flashdata('validation1', validation_errors());
+			$this->load->library('user_agent');
+
+
+            // Redirect the user to the previous page
+            redirect($this->agent->referrer());
+
+         } else {
+            // Validation passed, process the form data
+            $subcname = $this->input->post('subCat');
+            $catname = $this->input->post('catId');
+           
+            // Insert data into database
+            $data = array (
+                'subcname' => $subcname,
+                'catname' => $catname,
+                 );
+				
+				 $this->load->database();
+
+            $this->db->insert('subcategories', $data);
+            $this->session->set_flashdata('success', 'Record added successfully');
+            // $categories = $this->catmodel->get_categories();
+            // $data = array('categories' => $categories);
+            // $this->load->view('admin/categories', $data); 
+            redirect('admin/subcategories');  
+                 
+         }
+
+
+    }
 }
