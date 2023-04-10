8c5cf7818498ba0feb48bd953ae9bceba1eb90d5
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-04-10 11:18:14 +0600

    update categories by apon

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,8 +6,8 @@ class Admin extends CI_Controller {
 	public function __construct() {
         parent::__construct();
         $this->load->helper('form');
-        // $this->load->database();
-        // $this->load->model('usermodel');
+        $this->load->database();
+        $this->load->model('catmodel');
         $this->load->library('session');
         $this->load->helper('url');
     }
@@ -25,16 +25,51 @@ class Admin extends CI_Controller {
 	{
 
 
+		$categories = $this->catmodel->get_categories();
+		$data = array('categories' => $categories);
+		$this->load->view('admin/categories', $data);  
 
-		$this->load->view('admin/categories');
+
 	}
-	public function catstor()
-	{
+	public function catstor() {
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+		$this->form_validation->set_rules('catname', 'Category Name', 'required|max_length[12]|is_unique[categories.catname]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $categories = $this->catmodel->get_categories();
+            $data = array('categories' => $categories);
+            $this->load->view('user', $data);  
+
+         } else {
+            // Validation passed, process the form data
+            $catname = $this->input->post('catname');
+           
+            // Insert data into database
+            $data = array (
+                'catname' => $catname,
+                 );
+				
+				 $this->load->database();
+
+            $this->db->insert('categories', $data);
+            $this->session->set_flashdata('success', 'Record added successfully');
+            $categories = $this->catmodel->get_categories();
+            $data = array('categories' => $categories);
+            $this->load->view('admin/categories', $data);        
+         }
 
 
 
-		$this->load->view('admin/categories');
-	}
+         
+
+
+
+
+
+
+    }
 	public function subcategories()
 	{
 
