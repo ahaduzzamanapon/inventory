7281d4eada3c6c925763ab891d13776c403170a1
razibul-649 <razibul15-11649@diu.edu.bd>
2023-04-11 15:15:28 +0600

    marged

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -36,36 +36,24 @@ class Admin extends CI_Controller
 		$this->load->view('admin/categories', $data);  
 	}
 	public function catstor() {
-    $this->load->helper('url');
-    $this->load->helper('form');
-    $this->load->library('form_validation');
-    $this->form_validation->set_rules('catname', 'Category Name', 'required|max_length[12]|is_unique[categories.catname]');
+        $this->form_validation->set_rules('catname', 'Category Name', 'required|max_length[12]|is_unique[categories.catname]');
+        if ($this->form_validation->run() == FALSE) {
+			$this->session->set_flashdata('validation1', validation_errors());
+           // Redirect the user to the previous page
+            redirect($this->agent->referrer());
+        } else {
+            // Validation passed, process the form data
+            $catname = $this->input->post('catname');
 
-    if ($this->form_validation->run() == false) {
-        $this->session->set_flashdata('validation1', validation_errors());
-        $this->load->library('user_agent');
-
-
-        // Redirect the user to the previous page
-        redirect($this->agent->referrer());
-    } else {
-        // Validation passed, process the form data
-        $catname = $this->input->post('catname');
-
-        // Insert data into database
-        $data = array(
-            'catname' => $catname,
-             );
-
-        $this->load->database();
-
-        $this->db->insert('categories', $data);
-        $this->session->set_flashdata('success', 'Record added successfully');
-        $categories = $this->catmodel->get_categories();
-        $data = array('categories' => $categories);
-        $this->load->view('admin/categories', $data);
-    }
-}
+            // Insert data into database
+            $data = array(
+                'catname' => $catname,
+                 );
+                  $this->db->insert('categories', $data);
+                   $this->session->set_flashdata('success', 'Record update successfully');
+                   redirect($this->agent->referrer());
+                 }
+                 }
 
 
     
@@ -109,10 +97,6 @@ public function delete($id)
 
 
 
-
-
-
-    
 	public function subcategories()
 	{
 
@@ -241,6 +225,47 @@ public function delete_sub($sid){
 }
 
 
-   
-}
+    public function units()
+    {
 
+        $units = $this->Unit->get_units();
+
+
+
+        $data = array('units' => $units,
+                        'unitValidation1'=>$this->session->flashdata('unitValidation1'));
+        $this->load->view('admin/unit', $data);
+    }
+
+    public function unitStore()
+    {
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('unitName', 'Unit Name', 'required|max_length[12]|is_unique[units.unitName]');
+
+        if ($this->form_validation->run() == false) 
+        {
+            $this->session->set_flashdata('unitValidation1', validation_errors());
+            $this->load->library('user_agent');
+            
+            redirect($this->agent->referrer());  // Redirect the user to the previous page
+        } 
+        else {
+        
+            $unitName = $this->input->post('unitName');    // Validation passed, process the form data
+
+            // Insert data into database
+            $data = array(
+                'unitName' => $unitName,
+                 );
+
+            $this->load->database();
+            $this->db->insert('units', $data);
+            $this->session->set_flashdata('success', 'Unit added successfully');
+            $units = $this->Unit->get_units();
+            $data = array('units' => $units);
+            $this->load->view('admin/unit', $data);
+        }
+    }
+}
