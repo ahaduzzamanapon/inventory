322bf33bcae8939e3f00aa1f60d01c77ea973a5a
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-04-13 15:37:33 +0600

    updated by apon sql

--- inven.sql
+++ inven.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2023 at 07:41 AM
+-- Generation Time: Apr 13, 2023 at 11:33 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -37,9 +37,100 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `catname`) VALUES
-(1, 'catagory 2'),
-(2, 'catagory 2'),
-(3, 'catagory 1');
+(25, 'catagory 10'),
+(26, 'catagory 2'),
+(27, 'cat 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `itemname` varchar(255) NOT NULL,
+  `catid` int(11) NOT NULL,
+  `subid` int(11) NOT NULL,
+  `unitid` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `itemname`, `catid`, `subid`, `unitid`, `price`, `quantity`, `status`, `image`) VALUES
+(7, 'updatedfb', 26, 24, 5, 30, 33, 1, '6437b63353d3c.PNG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategories`
+--
+
+CREATE TABLE `subcategories` (
+  `sid` int(255) NOT NULL,
+  `subcname` varchar(255) NOT NULL,
+  `catname` varchar(200) NOT NULL,
+  `created_by` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subcategories`
+--
+
+INSERT INTO `subcategories` (`sid`, `subcname`, `catname`, `created_by`) VALUES
+(24, 'ghgdghd', '26', '2023-04-12 07:08:44.594676'),
+(25, 'jhfyh', '25', '2023-04-13 04:46:15.446591');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `id` int(11) NOT NULL,
+  `FirstName` varchar(150) DEFAULT NULL,
+  `LastName` varchar(150) DEFAULT NULL,
+  `Email` varchar(150) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `userType` int(2) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `FirstName`, `LastName`, `Email`, `Password`, `userType`, `created_at`) VALUES
+(1, 'mou', 'mita', 'mamun@gmail.com', 'Mamunkhan', 0, '2023-04-11 23:05:43'),
+(2, 'mou', 'sumi', 'mou@gmai.com', 'mou1234', 0, '2023-04-11 23:24:29'),
+(3, 'riad', 'ahamed', 'riad@gmail.com', '123456', 1, '2023-04-11 23:56:07'),
+(4, 'rion', 'rion', 'abc@g.com', '123456', 0, '2023-04-11 23:58:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `unitId` int(255) NOT NULL,
+  `unitName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `units`
+--
+
+INSERT INTO `units` (`unitId`, `unitName`) VALUES
+(4, 'Unit 1'),
+(5, 'Unit 2');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +143,30 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subcategories`
+--
+ALTER TABLE `subcategories`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`unitId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -59,7 +174,31 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `subcategories`
+--
+ALTER TABLE `subcategories`
+  MODIFY `sid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `units`
+--
+ALTER TABLE `units`
+  MODIFY `unitId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
